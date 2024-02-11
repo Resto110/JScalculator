3a44bed1dc3ab3be04ddefad9de260dfe57610a6
Resto110 <francesco.infoandjobs@gmail.com>
2024-02-11 13:48:56 +0100

    form created

---
+++ PHP/calculator_form.php
@@ -0,0 +1,11 @@
+<form id="form1" method="post" action="">
+    <input type="number" name="num1" required>
+    <select name="operator" required>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">&#215;</option>
+        <option value="/">/</option>
+    </select>
+    <input type="number" name="num2" required>
+    <input type="submit" value="Calculate">
+</form>
