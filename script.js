7ae7d145923a0156e03e330b5ff9131e7fb7b186
Resto110 <francesco.infoandjobs@gmail.com>
2024-02-29 05:43:27 +0100

    History scrapped

--- script.js
+++ script.js
@@ -49,9 +49,6 @@ function equal() {
 
     var buttons = document.getElementById('btn');
 
-    // Redirect to history page with expression as a query parameter
-    window.location.href = "history.php";
-
     // Divide by 0 checker 
     if (isNaN(lastResult)) {
       // Error Screen
